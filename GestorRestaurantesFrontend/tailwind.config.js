e05557887444bbf8dc197bafa9862e23a5f3397d
ValladaresCode <rvalladares-2024060@kinal.edu.gt>
2026-04-29 16:52:06 -0600

    C6. modificacion de estilos

--- GestorRestaurantesFrontend/tailwind.config.js
+++ GestorRestaurantesFrontend/tailwind.config.js
@@ -1,3 +1,13 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
