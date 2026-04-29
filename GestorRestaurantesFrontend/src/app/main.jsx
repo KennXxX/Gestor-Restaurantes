f601914941d04ee271bddbca990045b083542916
Marcos-JG <mgarcia-2024412@kinal.edu.gt>
2026-04-29 16:36:13 -0600

    C05 solucion de conflictos en el merge

--- GestorRestaurantesFrontend/src/app/main.jsx
+++ GestorRestaurantesFrontend/src/app/main.jsx
@@ -3,8 +3,7 @@ import { createRoot } from 'react-dom/client'
 import  App  from './App.jsx'
 import { ThemeProvider } from "@material-tailwind/react"
 import { BrowserRouter } from 'react-router-dom'
-import '../index.css'
-
+import '../styles/index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
